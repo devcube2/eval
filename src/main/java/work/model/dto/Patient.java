163e71a6383cccf4f4299097e0112f89b574610c
devcube2 <jejeje8@knou.ac.kr>
2025-02-25 11:06:54 +0900

    Initial commit

---
+++ src/main/java/work/model/dto/Patient.java
@@ -0,0 +1,10 @@
+package work.model.dto;
+
+public class Patient {
+    private int patientid;
+    private String name;
+    private String birthdate;
+    private String phone;
+    private String address;
+    private String createdat;
+}
---
+++ src/main/java/work/model/dto/doctor.java
@@ -0,0 +1,9 @@
+package work.model.dto;
+
+public class doctor {
+    private int doctorid;
+    private String name;
+    private String specialty;
+    private String phone;
+    private String createdat;
+}
